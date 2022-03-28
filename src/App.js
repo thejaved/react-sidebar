3abd4db46c9ebf2cab9c7931015cf158472da922
Javed Khan <live.javedkhan@gmail.com>
2022-03-28 19:34:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import "./App.css";
+import SideMenu, { menuItems } from "./components/SideMenu";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from "react";
+
+const Dashboard = () => <h1>Dashboard</h1>;
+const Content = () => <h1>Content</h1>;
+const Courses = () => <h1>Content/Courses</h1>;
+const Videos = () => <h1>Content/Videos</h1>;
+const Design = () => <h1>Design</h1>;
+const Content2 = () => <h1>Content2</h1>;
+const Courses2 = () => <h1>Content/Courses 2</h1>;
+const Videos2 = () => <h1>Content/Videos 2</h1>;
+const Design2 = () => <h1>Design 2</h1>;
+
+function App() {
+  const [inactive, setInactive] = useState(false);
+
+  return (
+    <div className="App">
+      <Router>
+        <SideMenu
+          onCollapse={(inactive) => {
+            console.log(inactive);
+            setInactive(inactive);
+          }}
+        />
+
+        <div className={`container ${inactive ? "inactive" : ""}`}>
+          {/* improvememt, not recorded in video, its just looping through menuItems
+          instead of hard coding all the routes */}
+          {menuItems.map((menu, index) => (
+            <>
+              <Route key={menu.name} exact={menu.exact} path={menu.to}>
+                <h1>{menu.name}</h1>
+              </Route>
+              {menu.subMenus && menu.subMenus.length > 0
+                ? menu.subMenus.map((subMenu, i) => (
+                    <Route key={subMenu.name} path={subMenu.to}>
+                      <h1>{subMenu.name}</h1>
+                    </Route>
+                  ))
+                : null}
+            </>
+          ))}
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
